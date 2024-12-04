393306170416054dc0dd4723c0a45a2126ba1f52
Erwin Mintiens <erwin.mintiens@protonmail.com>
2024-12-04 22:38:00 +0100

    Solved day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -126,7 +126,67 @@ fn check_x(x: usize, y: usize, vec: &Vec<&str>) -> u8 {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut result: u32 = 0;
+    let input_vec: Vec<&str> = input.split_terminator("\n").collect();
+    let mut positions = Vec::new();
+    for (y, s) in input_vec.iter().enumerate() {
+        for (x, v) in s.chars().enumerate() {
+            if v == 'M' {
+                positions.push((x, y));
+            }
+        }
+    }
+    for (x, y) in positions {
+        result += check_m(x, y, &input_vec) as u32;
+    }
+    Some(result)
+}
+
+fn check_m(x: usize, y: usize, vec: &Vec<&str>) -> u8 {
+    let mut result: u8 = 0;
+    if vec[y].chars().nth(x).unwrap() != 'M' {
+        eprintln!("{}", vec[x].chars().nth(y).unwrap());
+        panic!("The given coordinates do not point to an X")
+    }
+    // Check M below
+    if y + 2 < vec.len() && vec[y + 2].chars().nth(x).unwrap() == 'M' {
+        // Check A to the right
+        if x + 2 < vec[0].len() && vec[y + 1].chars().nth(x + 1).unwrap() == 'A' {
+            if vec[y].chars().nth(x + 2).unwrap() == 'S'
+                && vec[y + 2].chars().nth(x + 2).unwrap() == 'S'
+            {
+                result += 1;
+            }
+        }
+        // Check A to the left
+        if !(0..2).contains(&x) && vec[y + 1].chars().nth(x - 1).unwrap() == 'A' {
+            if vec[y].chars().nth(x - 2).unwrap() == 'S'
+                && vec[y + 2].chars().nth(x - 2).unwrap() == 'S'
+            {
+                result += 1;
+            }
+        }
+    }
+    // Check M to the right
+    if x + 2 < vec[0].len() && vec[y].chars().nth(x + 2).unwrap() == 'M' {
+        // Check A below
+        if y + 2 < vec.len() && vec[y + 1].chars().nth(x + 1).unwrap() == 'A' {
+            if vec[y + 2].chars().nth(x).unwrap() == 'S'
+                && vec[y + 2].chars().nth(x + 2).unwrap() == 'S'
+            {
+                result += 1;
+            }
+        }
+        // Check A above
+        if !(0..2).contains(&y) && vec[y - 1].chars().nth(x + 1).unwrap() == 'A' {
+            if vec[y - 2].chars().nth(x).unwrap() == 'S'
+                && vec[y - 2].chars().nth(x + 2).unwrap() == 'S'
+            {
+                result += 1;
+            }
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -142,6 +202,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
